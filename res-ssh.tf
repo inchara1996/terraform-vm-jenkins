b8dd807315082131478f93491ea2e3cab642d3fb
inchara1996 <incharanbhushan1996@gmail.com>
2026-01-16 23:47:37 +0530

    Update res-ssh.tf

--- res-ssh.tf
+++ res-ssh.tf
@@ -4,9 +4,10 @@ resource "azurerm_ssh_public_key" "ssh-key" {
   name                = "inch-ssh"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  public_key          = file("C:/Users/admin/.ssh")
+  public_key          = file("C:/Users/admin/.ssh/id_rsa.pub")
 
 }
+
 
 
 
